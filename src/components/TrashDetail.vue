f18a87448d82da15e237692e1341c20d85ea7c90
hefeixiong <1074535830@qq.com>
2022-01-20 15:12:52 +0800

    新增四个基础组件

---
+++ src/components/TrashDetail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="login">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'This is TrashDetail page'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
+
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login.vue'
+import NotebookList from '@/components/NotebookList.vue'
+import NoteDetail from '@/components/NoteDetail.vue'
+import TrashDetail from '@/components/TrashDetail.vue'
+
+
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +20,21 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/notebooks',
+      name: 'notebooks',
+      component: NotebookList
+    },
+    {
+      path: '/note/:noteId',
+      name: 'note',
+      component: NoteDetail
+    },
+    {
+      path: '/trash/:noteId',
+      name: 'trash',
+      component: TrashDetail
     }
   ]
 })
