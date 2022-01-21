d5fdc97e66c31eaadbab1da270323900b722e083
hefeixiong <1074535830@qq.com>
2022-01-21 16:50:29 +0800

    封装notes API

---
+++ src/apis/notes.js
@@ -0,0 +1,44 @@
+import request from '@/helpers/request'
+import {friendlyDate} from '@/helpers/util'
+
+const URL = {
+  GET: '/notes/from/:notebookId',
+  ADD: '/notes/to/:notebookId',
+  UPDATE: '/notes/:noteId',
+  DELETE: '/notes/:noteId'
+}
+
+export default {
+  getAll ({notebookId}) {
+    return new Promise((resolve, reject) => {
+      request(URL.GET.replace(':notebookId', notebookId))
+        .then(res => {
+          res.data = res.data.map(note => {
+            note.createAtFriendly = friendlyDate(note.createdAt)
+            note.updateAtFriendlu = friendlyDate(note.updatedAt)
+            return note
+          }).sort((note1, note2) => {
+            return note1.updatedAt < note2.updatadAt
+          })
+          resolve(res)
+        }).catch(err => reject(err))
+    })
+  },
+  updateNote ({noteId}, {title, content}) {
+    return request(URL.UPDATE.replace(':noteId', noteId), 'PATH', {title, content})
+  },
+  deleteNote ({noteId}) {
+    return request(URL.DELETE.replace(':noteId', noteId), 'DELETE')
+  },
+
+  addNote ({notebookId}, {title = '', content = ''} = {title: '', content: ''}) {
+    return new Promise((resolve, reject) => {
+      request(URL.ADD.replace(':notebookId', notebookId), 'POST', {title, content})
+        .then(res => {
+          res.data.createdAtFriendly = friendlyDate(res.data.createdAt)
+          res.data.updatedAtFriendly = friendlyDate(res.data.updatedAt)
+          resolve(res)
+        }).catch(err => reject(err))
+    })
+  }
+}
