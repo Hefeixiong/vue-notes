9d938bc9ea3171a80d5373b9ffe417ba46651e34
hefeixiong <1074535830@qq.com>
2022-01-23 17:07:08 +0800

    NoteDetail

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,35 +1,110 @@
 <template>
-  <div id="login">
-    <h1>{{ msg }}</h1>
+  <div id="note" class="detail">
+    <note-sidebar @update:notes="val => notes = val"></note-sidebar>
+    <div class="note-detail">
+      <div class="note-empty" v-show="!curBook.id">请创建笔记本后</div>
+      <div class="note-empty" v-show="!curNote.id">选择或创建笔记本</div>
+      <div class="note-detail-ct" v-show="!curNote.id">
+        <div class="note-bar">
+          <span>创建日期：{{curNote.createAtFriendly}}</span>
+          <span>更新日期：{{curNote.updatedAtFriendly}}</span>
+          <span>{{statusText}}</span>
+          <span class="iconfont icon-delete"></span>
+          <span class="iconfont" :class="isShowPreview?'icon-edit' : 'icon-eye'" @click="isShowPreview = !isShowPreview"></span>
+        </div>
+        <div class="note-title">
+          <input type="text" v-model="curNote.title" @input="onUpdateNote" @keydown="statusText='正在输入...'" placeholder="输入标题">
+        </div>
+        <div class="editor">
+          <codemirror v-model="curNote.content" :options="cmOptions" v-show="!isShowPreview" @input="onUpdateNote" @inputRead="statusText='正在输入...'"></codemirror>
+          <div class="preview markdown-body" v-html="previewContent" v-show="isShowPreview"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Auth from '@/apis/auth'
-import Bus from '@/helpers/bus'
+import NoteSidebar from './NoteSidebar'
+import MarkdownIt from 'markdown-it'
+import _ from 'lodash'
+import {mapActions, mapGetters, mapMutations} from 'vuex'
+import { codemirror } from 'vue-codemirror'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/markdown/markdown.js'
+import 'codemirror/theme/neat.css'
+
+let md = new MarkdownIt()
 
 export default {
-  name: 'Login',
+  components: {
+    NoteSidebar,
+    codemirror
+  },
   data () {
     return {
-      msg: 'This is NoteDetail page'
+      statusText: '笔记未改动',
+      isShowPreview: false,
+      cmOptions: {
+        tabSize: 4,
+        mode: 'text/x-markdown',
+        theme: 'neat',
+        lineNumber: false,
+        line: true
+      }
     }
   },
   created () {
-    Bus.$on('userInfo', user => {
-      console.log(user.username)
-      Auth.getInfo().then(res => {
-        if (!res.isLogin) {
-          this.$router.push({path: '/login'})
-        }
+    this.checkLogin({path: '/login'})
+  },
+  computed: {
+    ...mapGetters([
+      'notes',
+      'curBook',
+      'curNote'
+    ]),
+    previewContent () {
+      return md.render(this.curNote.content || '')
+    }
+  },
+  methods: {
+    ...mapMutations([
+      'setCurNote'
+    ]),
+    ...mapActions([
+      'updateNote',
+      'deleteNote',
+      'checkLogin'
+    ]),
+    onUpdateNote: _.debounce(function () {
+      if (!this.curNote.id) return
+      this.updateNote({noteId: this.curNote.id, title: this.curNote.title, content: this.curNote.content})
+        .then(date => {
+          this.statusText = '已保存'
+        }).catch(date => {
+          this.statusText = '保存出错'
+        })
+    }, 3000),
+    onDeleteNote () {
+      this.deleteNote({noteId: this.curNote.id}).then(data => {
+        this.$router.replace({path: '/note'})
       })
-    })
+    }
+  },
+  beforeRouteUpdate (to, from, next) {
+    this.setCurNote({curNoteId: to.query.noteId})
+    next()
   }
 }
 </script>
 
-<style scoped>
-h1 {
-  color: red;
+<style lang="less">
+@import url(../assets/css/note-detail.less);
+
+#note {
+  display: flex;
+  align-items: stretch;
+  background-color: #fff;
+  flex: 1;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user'
 import notebook from './modules/notebook'
+import note from './modules/note'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     user,
-    notebook
+    notebook,
+    note
   }
 })
